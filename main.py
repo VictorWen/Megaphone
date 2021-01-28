27a009c6b50aaf88d555fe8694fd1524edb77cdc
Victor Wen <vicwen02@g.ucla.edu>
2021-01-28 05:01:47 +0000

    Added a enabled and Disable feature. Reorganized utils.play_audio

--- main.py
+++ main.py
@@ -12,79 +12,101 @@ client = commands.Bot(command_prefix='*')
 
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+  print('Logged in as {0.user}'.format(client))
 
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    if not member.bot and str(member.id) not in utils.get_blacklist(member.guild) and after.channel is not None and before.channel != after.channel:
-        print("Detected {0} joined a channel in {1}".format(member, member.guild))
-        await utils.play_audio(member, after.channel)
+  # Check if not a bot, is not in blacklist, has fanfare enabled, and has moved to a channel
+  if not member.bot and str(member.id) not in utils.get_blacklist(member.guild) and utils.get_data(member.guild, member, "enabled") != "false" and after.channel is not None and before.channel != after.channel:
+    print("Detected {0} joined a channel in {1}".format(member, member.guild))
+    await utils.play_audio(member, after.channel)
 
 
 @client.command()
 async def fanfare(context, url, start = None, length = None):
-    msg = ""
-    if validators.url(url):
-        msg += "Successfully added new fanfare for {0.mention}"
-        utils.set_data(context.guild, context.author, "url", url)
-        if start and (start.isnumeric() or start.replace('.', '', 1).isdigit()) and float(start) > 0:
-            utils.set_data(context.guild, context.author, "start", start)
-            msg += " starting at " + start + " seconds"
-            if length and (length.isnumeric() or length.replace('.', '', 1).isdigit()) and float(length) > 0:
-                utils.set_data(context.guild, context.author, "length", length)
-                msg += " and lasting " + length + " seconds"
-            else:
-                utils.set_data(context.guild, context.author, "length", None)
-        else:
-            utils.set_data(context.guild, context.author, "start", None)
+  msg = ""
+  # check if url is a valid URL
+  if validators.url(url):
+    msg += "Successfully added new fanfare for {0.mention}"
+    utils.set_data(context.guild, context.author, "url", url)
+    # Check if start can be converted to a float and greater than zero
+    if start and (start.isnumeric() or start.replace('.', '', 1).isdigit()) and float(start) >= 0:
+      utils.set_data(context.guild, context.author, "start", start)
+      msg += " starting at " + start + " seconds"
 
-        await context.send(msg.format(context.author))
-        print("Added new fanfare for: {0.name}".format(context.author))
+      # Check if length can be converted to a float and greater than zero
+      if length and (length.isnumeric() or length.replace('.', '', 1).isdigit()) and float(length) >= 0:
+        utils.set_data(context.guild, context.author, "length", length)
+        msg += " and lasting " + length + " seconds"
+      else:
+        utils.set_data(context.guild, context.author, "length", None)
     else:
-        await context.send("Not a valid URL")   
-        return         
-    
+      utils.set_data(context.guild, context.author, "start", None)
+      utils.set_data(context.guild, context.author, "length", None)
+
+    await context.send(msg.format(context.author))
+    print("Added new fanfare for: {0.name}".format(context.author))
+  else:
+    await context.send("Not a valid URL")   
+    return         
 
 
 @client.command()
 async def play(context):
-    if not context.author.voice:
-        await context.send("You are not in a voice channel")
-        return
-    if str(context.author.id) not in utils.get_blacklist(context.guild):
-        await context.send("Playing {0}'s fanfare".format(context.author.mention))
-        await utils.play_audio(context.author, context.author.voice.channel)
+  if not context.author.voice:
+    await context.send("You are not in a voice channel")
+    return
+  if utils.get_data(context.guild, context.author, "enabled") == "false":
+    await context.send("You do not have fanfare enabled, use \'*enable\' to enable it")
+    return
+  if str(context.author.id) not in utils.get_blacklist(context.guild):
+    await context.send("Playing {0}'s fanfare".format(context.author.mention))
+    await utils.play_audio(context.author, context.author.voice.channel)
 
 
 @client.command()
 async def blacklist(context):
-    if str(context.author.id) in utils.get_blacklist(context.guild):
-        return
-    permissions = context.author.guild_permissions
-    if permissions.mute_members or \
-            permissions.administrator:
-        guild_blacklist = utils.get_blacklist(context.guild)
-        for user in context.message.mentions:
-            if str(user.id) not in guild_blacklist:
-                guild_blacklist.append(str(user.id))
-                await context.send("Added {0} to the blacklist".format(user.mention))
-        utils.save_data()
+  if str(context.author.id) in utils.get_blacklist(context.guild):
+    return
+  permissions = context.author.guild_permissions
+  if permissions.mute_members or permissions.administrator:
+    guild_blacklist = utils.get_blacklist(context.guild)
+    for user in context.message.mentions:
+      if str(user.id) not in guild_blacklist:
+        guild_blacklist.append(str(user.id))
+        await context.send("Added {0} to the blacklist".format(user.mention))
+    utils.save_data()
 
 
 @client.command()
 async def whitelist(context):
-    if str(context.author.id) in utils.get_blacklist(context.guild):
-        return
-    permissions = context.author.guild_permissions
-    if permissions.mute_members or \
-            permissions.administrator:
-        guild_blacklist = utils.get_blacklist(context.guild)
-        for user in context.message.mentions:
-            if str(user.id) in guild_blacklist:
-                guild_blacklist.remove(str(user.id))
-                await context.send("Removed {0} to the blacklist".format(user.mention))
-        utils.save_data()
+  if str(context.author.id) in utils.get_blacklist(context.guild):
+    return
+  permissions = context.author.guild_permissions
+  if permissions.mute_members or permissions.administrator:
+      guild_blacklist = utils.get_blacklist(context.guild)
+      for user in context.message.mentions:
+        if str(user.id) in guild_blacklist:
+          guild_blacklist.remove(str(user.id))
+          await context.send("Removed {0} to the blacklist".format(user.mention))
+      utils.save_data()
+
+
+@client.command()
+async def disable(context):
+  if utils.get_data(context.guild, context.author, "enabled") != "false":
+    utils.set_data(context.guild, context.author, "enabled", "false")
+    print("Disabling for {0}".format(context.author))
+    await context.send("Disabled fanfare for {0}".format(context.author.mention))
+
+
+@client.command()
+async def enable(context):
+  if utils.get_data(context.guild, context.author, "enabled") == "false":
+    utils.set_data(context.guild, context.author, "enabled", "true")
+    print("Enabling for {0}".format(context.author))
+    await context.send("Enabled fanfare for {0}".format(context.author.mention))
 
 
 keep_alive()
--- utils.py
+++ utils.py
@@ -6,18 +6,18 @@ import time
 
 # Constants
 ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address':
-    '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
+  'format': 'bestaudio/best',
+  'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+  'restrictfilenames': True,
+  'noplaylist': True,
+  'nocheckcertificate': True,
+  'ignoreerrors': False,
+  'logtostderr': False,
+  'quiet': True,
+  'no_warnings': True,
+  'default_search': 'auto',
+  'source_address':
+  '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
@@ -26,108 +26,132 @@ PLAYTIME = 10
 FALLOFF = 10
 
 connection_queue = {}
-play_starts = {}
+play_id = {}
 
 read_file = open("./data", "r")
 json_str = read_file.read()
 if json_str != "":
-    data = json.loads(json_str)
+  data = json.loads(json_str)
 else:
-    data = {}
+  data = {}
 read_file.close()
 
 
 async def play_audio(member: discord.User, channel: discord.VoiceChannel):
 
-    # Connect to the voice client
-    if member.guild.id not in connection_queue:
-        connection_queue[member.guild.id] = []
-    connect_id = str(time.time())
-    connection_queue[member.guild.id].append(connect_id)
-    while connection_queue[member.guild.id][0] != connect_id:
-        await asyncio.sleep(1)
-    vc = member.guild.voice_client
-    if not vc or not vc.is_connected():
-        vc = await channel.connect()
-    elif vc.channel != channel:
-        await vc.move_to(channel)
-    print("Joined {0}".format(channel))
+  # Connect to the voice client
+  # Wait for queue
+  if member.guild.id not in connection_queue:
+    connection_queue[member.guild.id] = []
+  connect_id = str(time.time())
+  connection_queue[member.guild.id].append(connect_id)
+  while connection_queue[member.guild.id][0] != connect_id:
     await asyncio.sleep(1)
-    connection_queue[member.guild.id].pop(0)
+  vc = member.guild.voice_client
+  if not vc or not vc.is_connected():
+    vc = await channel.connect()
+  elif vc.channel != channel:
+    await vc.move_to(channel)
+  print("Joined {0}".format(channel))
+  
+  # Use the time as an id for synchronization
+  play_id[member.guild.id] = str(time.time())
+  await asyncio.sleep(1)
+  connection_queue[member.guild.id].pop(0)
 
-    # Obtain the URL for the youtube video audio
-    print("Loading Audio...")
+  # Obtain the URL for the youtube video audio
+  print("Loading Audio...")
+  url = get_data(member.guild, member, "url")
+  url = url if url else "https://www.youtube.com/watch?v=x_XVntliea0"  # default URL
 
-    url = get_data(member.guild, member, "url")
-    url = url if url else "https://www.youtube.com/watch?v=x_XVntliea0"  # default URL
+  # Stream the audio from the youtube video as an audio player
+  # IDK what this really does
+  audio_data = await asyncio.get_event_loop().run_in_executor(
+    None, lambda: ytdl.extract_info(url, download=False))
+  filename = audio_data['url']
+  duration = float(audio_data['duration'])
 
-    ffmpeg_options = {'options': '-vn'}
-    start = get_data(member.guild, member, "start")
-    if start:
-      ffmpeg_options["options"] += " -ss " + start
-    length = get_data(member.guild, member, "length")
-    length = min(float(length), PLAYTIME + FALLOFF) if length else PLAYTIME + FALLOFF
-    ffmpeg_options["options"] += " -t " + str(length + 1);
+  # Set options for start time and length
+  ffmpeg_options = {'options': '-vn'}
+  start = get_data(member.guild, member, "start")
+  if start:
+    ffmpeg_options["options"] += " -ss " + start
+  length = get_data(member.guild, member, "length")
+  length = min(float(length), PLAYTIME + FALLOFF) if length else PLAYTIME + FALLOFF
+  if length < duration:
+    ffmpeg_options["options"] += " -t " + str(length)
+  else:
+    length = duration
+  length += 0.5
 
-    # Stream the audio from the youtube video as an audio player
-    # IDK what this really does
-    audio_data = await asyncio.get_event_loop().run_in_executor(
-        None, lambda: ytdl.extract_info(url, download=False))
-    filename = audio_data['url']
-    audio = discord.FFmpegPCMAudio(filename, **ffmpeg_options)
-    audio_player = discord.PCMVolumeTransformer(audio)
+  # Create discord audio player
+  audio = discord.FFmpegPCMAudio(filename, **ffmpeg_options)
+  audio_player = discord.PCMVolumeTransformer(audio)
 
-    # Play the audio
-    if vc and vc.is_connected():
-        if vc.is_playing():
-            vc.stop()
-        print("Playing audio")
-        vc.play(audio_player)
-        
-        start_time = str(time.time())
-        play_starts[member.guild.id] = start_time
-        play_length = min(PLAYTIME, length)
-        await asyncio.sleep(play_length + 0.5)
-        if start_time == play_starts[member.guild.id]:
-            falloff_time = length - play_length
-            while time.time() - float(play_starts[member.guild.id]) - play_length < falloff_time:
-                audio_player.volume -= (0.1 / FALLOFF)
-                await asyncio.sleep(0.1)
-                if play_starts[member.guild.id] != start_time:
-                    return
-            audio_player.volume = 0
-            print("Finished playing audio")
-            vc.stop()
-            await asyncio.sleep(0.1)
-            await vc.disconnect()
-            print("Successfully disconnected")
-            vc.cleanup()
+  # Play the audio
+  if vc and vc.is_connected():
+    if vc.is_playing():
+      vc.stop()
+    print("Playing audio")
+    vc.play(audio_player)
+    
+    start_time = str(time.time())
+    play_id[member.guild.id] = start_time
+    play_length = min(PLAYTIME, length)
+
+    # normal play
+    while time.time() - float(play_id[member.guild.id]) < play_length:
+      await asyncio.sleep(0.01)
+      if play_id[member.guild.id] != start_time:
+        # If the play id has changed, stop
+        return
+
+    # volume falloff
+    falloff_time = length - play_length
+    while time.time() - float(play_id[member.guild.id]) - play_length < falloff_time:
+      audio_player.volume -= (0.01 / FALLOFF)
+      await asyncio.sleep(0.01)
+      if play_id[member.guild.id] != start_time:
+          # If the play id has changed, stop falloff
+          return
+      
+    # Finish playing
+    audio_player.volume = 0
+    print("Finished playing audio")
+    await asyncio.sleep(0.5)
+    # If the same play id, disconnect
+    if play_id[member.guild.id] == start_time:
+      vc.stop()
+      await asyncio.sleep(0.01)
+      await vc.disconnect()
+      print("Successfully disconnected")
+      vc.cleanup()
 
 
 def get_data(guild: discord.Guild, member: discord.User, tag: str):
-    if str(guild.id) in data and \
-    str(member.id) in data[str(guild.id)] and \
-    str(tag) in data[str(guild.id)][str(member.id)]:
-        return data[str(guild.id)][str(member.id)][str(tag)]
-    else:
-        return None
+  if str(guild.id) in data and \
+  str(member.id) in data[str(guild.id)] and \
+  str(tag) in data[str(guild.id)][str(member.id)]:
+    return data[str(guild.id)][str(member.id)][str(tag)]
+  else:
+    return None
 
 
 def set_data(guild: discord.Guild, member: discord.User, tag: str, datum: str):
-    if str(guild.id) not in data:
-        data[str(guild.id)] = {}
-    if str(member.id) not in data[str(guild.id)]:
-        data[str(guild.id)][str(member.id)] = {}
-    data[str(guild.id)][str(member.id)][str(tag)] = datum
-    save_data()
+  if str(guild.id) not in data:
+    data[str(guild.id)] = {}
+  if str(member.id) not in data[str(guild.id)]:
+    data[str(guild.id)][str(member.id)] = {}
+  data[str(guild.id)][str(member.id)][str(tag)] = datum
+  save_data()
 
 
 def get_blacklist(guild: discord.Guild):
-    if str(guild.id) not in data:
-        data[str(guild.id)] = {}
-    if "blacklist" not in data[str(guild.id)]:
-        data[str(guild.id)]["blacklist"] = []
-    return data[str(guild.id)]["blacklist"]
+  if str(guild.id) not in data:
+    data[str(guild.id)] = {}
+  if "blacklist" not in data[str(guild.id)]:
+    data[str(guild.id)]["blacklist"] = []
+  return data[str(guild.id)]["blacklist"]
 
 
 # TEMP: Updates the data to the new format
@@ -142,7 +166,7 @@ def get_blacklist(guild: discord.Guild):
 
 
 def save_data():
-    write_file = open("./data", "w")
-    write_json_str = json.dumps(data)
-    write_file.write(write_json_str)
-    write_file.close()
+  write_file = open("./data", "w")
+  write_json_str = json.dumps(data)
+  write_file.write(write_json_str)
+  write_file.close()
