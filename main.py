970dd3719afc66c134ed6cb1e070913ae47bfffa
Victor Wen <vicwen02@g.ucla.edu>
2021-02-01 21:38:54 +0000

    Added some documentation. Added embedded messages. Added some more connection guards.

--- main.py
+++ main.py
@@ -22,91 +22,135 @@ async def on_voice_state_update(member, before, after):
     await utils.play_audio(member, after.channel)
 
 
-@client.command()
-async def fanfare(context, url, start = None, length = None):
-  msg = ""
-  # check if url is a valid URL
-  if validators.url(url):
-    msg += "Successfully added new fanfare for {0.mention}"
-    await utils.set_data(context.guild, context.author, "url", url)
-    # Check if start can be converted to a float and greater than zero
-    if start and (start.isnumeric() or start.replace('.', '', 1).isdigit()) and float(start) >= 0:
-      await utils.set_data(context.guild, context.author, "start", start)
-      msg += " starting at " + start + " seconds"
+class Fanfare(commands.Cog):
+  
+  @commands.command()
+  async def fanfare(self, ctx, yt_url, start = None, length = None):
+    '''
+    Sets the fanfare for the user.
+    Parameters:
+      yt_url: a valid URL to a Youtube video.
+      start: the starting time of the fanfare in seconds.
+      length: the duration of the fanfare in seconds.
+    '''
+    msg = ""
+    # check if url is a valid URL
+    if validators.url(yt_url):
+      msg += "Successfully added new fanfare for {0.mention}"
+      await utils.set_data(ctx.guild, ctx.author, "url", yt_url)
+      # Check if start can be converted to a float and greater than zero
+      if start and (start.isnumeric() or start.replace('.', '', 1).isdigit()) and float(start) >= 0:
+        await utils.set_data(ctx.guild, ctx.author, "start", start)
+        msg += " starting at " + start + " seconds"
 
-      # Check if length can be converted to a float and greater than zero
-      if length and (length.isnumeric() or length.replace('.', '', 1).isdigit()) and float(length) >= 0:
-        await utils.set_data(context.guild, context.author, "length", length)
-        msg += " and lasting " + length + " seconds"
+        # Check if length can be converted to a float and greater than zero
+        if length and (length.isnumeric() or length.replace('.', '', 1).isdigit()) and float(length) >= 0:
+          await utils.set_data(ctx.guild, ctx.author, "length", length)
+          msg += " and lasting " + length + " seconds"
+        else:
+          await utils.set_data(ctx.guild, ctx.author, "length", None)
       else:
-        await utils.set_data(context.guild, context.author, "length", None)
+        await utils.set_data(ctx.guild, ctx.author, "start", None)
+        await utils.set_data(ctx.guild, ctx.author, "length", None)
+
+      # embed = discord.Embed(description = )
+      msg += "."
+      await utils.send_embed(ctx, msg.format(ctx.author), color = 0x009900)
+      print("Added new fanfare for: {0.name}".format(ctx.author))
     else:
-      await utils.set_data(context.guild, context.author, "start", None)
-      await utils.set_data(context.guild, context.author, "length", None)
-
-    await context.send(msg.format(context.author))
-    print("Added new fanfare for: {0.name}".format(context.author))
-  else:
-    await context.send("Not a valid URL")   
-    return         
+      await utils.send_embed(ctx, f"{yt_url} is not a valid URL!", color = 0xff0000) 
+      return         
 
 
-@client.command()
-async def play(context):
-  if not context.author.voice:
-    await context.send("You are not in a voice channel")
-    return
-  if utils.get_data(context.guild, context.author, "enabled") == "false":
-    await context.send("You do not have fanfare enabled, use \'*enable\' to enable it")
-    return
-  if str(context.author.id) not in utils.get_blacklist(context.guild):
-    await context.send("Playing {0}'s fanfare".format(context.author.mention))
-    await utils.play_audio(context.author, context.author.voice.channel)
+  @commands.command()
+  async def play(self, ctx):
+    '''
+    Plays the fanfare of the user.
+    Requires that the user be in a voice channel.
+    '''
+    if not ctx.author.voice:
+      await utils.send_embed(ctx, "You are not in a voice channel!", color = 0xff0000)
+      return
+    # if utils.get_data(context.guild, context.author, "enabled") == "false":
+    #   await utils.send_embed(context,"You do not have fanfare enabled! Use \'*enable\' to enable it.", color = 0xff0000)
+    #   return
+    if str(ctx.author.id) not in utils.get_blacklist(ctx.guild):
+      await utils.send_embed(ctx, "Playing {0}'s fanfare.".format(ctx.author.mention))
+      await utils.play_audio(ctx.author, ctx.author.voice.channel)
+    else:
+      await utils.send_embed(ctx, "You are blacklisted and cannot play fanfare.", color = 0)
 
 
-@client.command()
-async def blacklist(context):
-  if str(context.author.id) in utils.get_blacklist(context.guild):
-    return
-  permissions = context.author.guild_permissions
-  if permissions.mute_members or permissions.administrator:
-    guild_blacklist = utils.get_blacklist(context.guild)
-    for user in context.message.mentions:
-      if str(user.id) not in guild_blacklist:
-        guild_blacklist.append(str(user.id))
-        await context.send("Added {0} to the blacklist".format(user.mention))
-    await utils.save_data()
+class AdminSettings(commands.Cog):
 
-
-@client.command()
-async def whitelist(context):
-  if str(context.author.id) in utils.get_blacklist(context.guild):
-    return
-  permissions = context.author.guild_permissions
-  if permissions.mute_members or permissions.administrator:
-      guild_blacklist = utils.get_blacklist(context.guild)
-      for user in context.message.mentions:
-        if str(user.id) in guild_blacklist:
-          guild_blacklist.remove(str(user.id))
-          await context.send("Removed {0} to the blacklist".format(user.mention))
+  @commands.command()
+  async def blacklist(self, ctx):
+    '''
+    Blacklists any mentioned members.
+    This prevents them from playing their fanfare.
+    The user must either have server mute permissions or be an administrator.
+    The user must also not be blacklisted.
+    '''
+    if str(ctx.author.id) in utils.get_blacklist(ctx.guild):
+      return
+    permissions = ctx.author.guild_permissions
+    if permissions.mute_members or permissions.administrator:
+      guild_blacklist = utils.get_blacklist(ctx.guild)
+      for user in ctx.message.mentions:
+        if str(user.id) not in guild_blacklist:
+          guild_blacklist.append(str(user.id))
+          await utils.send_embed(ctx, "Added {0} to the blacklist.".format(user.mention), color = 0)
       await utils.save_data()
 
 
-@client.command()
-async def disable(context):
-  if utils.get_data(context.guild, context.author, "enabled") != "false":
-    await utils.set_data(context.guild, context.author, "enabled", "false")
-    print("Disabling for {0}".format(context.author))
-    await context.send("Disabled fanfare for {0}".format(context.author.mention))
+  @commands.command()
+  async def whitelist(self, ctx):
+    '''
+    Whitelists any mentioned members.
+    This allows them to play their fanfare.
+    The user must either have server mute permissions or be an administrator.
+    The user must also not be blacklisted.
+    '''
+    if str(ctx.author.id) in utils.get_blacklist(ctx.guild):
+      return
+    permissions = ctx.author.guild_permissions
+    if permissions.mute_members or permissions.administrator:
+        guild_blacklist = utils.get_blacklist(ctx.guild)
+        for user in ctx.message.mentions:
+          if str(user.id) in guild_blacklist:
+            guild_blacklist.remove(str(user.id))
+            await utils.send_embed(ctx, "Removed {0} to the blacklist.".format(user.mention), color = 0xffffff)
+        await utils.save_data()
 
 
-@client.command()
-async def enable(context):
-  if utils.get_data(context.guild, context.author, "enabled") == "false":
-    await utils.set_data(context.guild, context.author, "enabled", "true")
-    print("Enabling for {0}".format(context.author))
-    await context.send("Enabled fanfare for {0}".format(context.author.mention))
+class UserSettings(commands.Cog):
+  
+  @commands.command()
+  async def disable(self, ctx):
+    '''
+    Disables the user's fanfare.
+    Makes it so that fanfare will not play when they join a voice channel.
+    '''
+    if utils.get_data(ctx.guild, ctx.author, "enabled") != "false":
+      await utils.set_data(ctx.guild, ctx.author, "enabled", "false")
+      print("Disabling for {0}".format(ctx.author))
+      await utils.send_embed(ctx, "Disabled fanfare for {0}.".format(ctx.author.mention))
+
+
+  @commands.command()
+  async def enable(self, ctx):
+    '''
+    Enables the user's fanfare.
+    Makes it so that fanfare will play when they join a voice channel.
+    '''
+    if utils.get_data(ctx.guild, ctx.author, "enabled") == "false":
+      await utils.set_data(ctx.guild, ctx.author, "enabled", "true")
+      print("Enabling for {0}".format(ctx.author))
+      await utils.send_embed(ctx,"Enabled fanfare for {0}.".format(ctx.author.mention))
 
 
 keep_alive()
+client.add_cog(Fanfare())
+client.add_cog(UserSettings())
+client.add_cog(AdminSettings())
 client.run(config('TOKEN'))
--- utils.py
+++ utils.py
@@ -17,6 +17,8 @@ ytdl_format_options = {
   'quiet': True,
   'no_warnings': True,
   'default_search': 'auto',
+  'skip_download': True,
+  'simulate': True,
   'source_address':
   '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
@@ -43,20 +45,39 @@ async def play_audio(member: discord.User, channel: discord.VoiceChannel):
   play_id = str(time.time())
   play_ids[member.guild.id] = play_id
 
+  await asyncio.sleep(0.5)
+  if (play_ids[member.guild.id] != play_id):
+    print(f"{play_id}: Connection to {channel} in {member.guild} aborted")
+    return
+
   # Connect the voice client
   try:
     vc = member.guild.voice_client
     if not vc:
       print(f"{play_id}: Connecting...")
       vc = await channel.connect(timeout = 5)
-      print(f"{play_id}: Connected to {channel}")
+      print(f"{play_id}: Connected to {channel} in {member.guild}")
     elif vc.channel != channel:
-      print(f"{play_id}: Moving to {channel}")
+      print(f"{play_id}: Moving to {channel} in {member.guild}")
       await vc.move_to(channel)
-  except:
+  except Exception as e:
     # connection_queue[member.guild.id].remove(connect_id)
     print(f"{play_id}: Failed to conncect to {channel} in {member.guild}")
+    raise e
     return
+
+  # Ensure proper connection before continuing
+  timeout = 0
+  while not vc.is_connected():
+    timeout += 1
+    print(f"{play_id}: Waiting for Voice Client connection, attempt #{timeout}")
+    await asyncio.sleep(1)
+    if (timeout > 5):
+      print(f"{play_id}: Failed to conncect to Voice Client")
+      if play_ids[member.guild.id] == play_id:
+        await vc.disconnect(force = True)
+        print(f"{play_id}: Successfully disconnected")
+      return
 
   # Obtain the URL for the youtube video audio
   print(f"{play_id}: Loading Audio...")
@@ -65,6 +86,7 @@ async def play_audio(member: discord.User, channel: discord.VoiceChannel):
 
   # Stream the audio from the youtube video as an audio player
   try:
+    # Load the audio from Youtube-DL
     audio_data = await asyncio.get_event_loop().run_in_executor(
       None, lambda: ytdl.extract_info(url, download=False))
     filename = audio_data['url']
@@ -75,6 +97,7 @@ async def play_audio(member: discord.User, channel: discord.VoiceChannel):
     start = get_data(member.guild, member, "start")
     if start:
       ffmpeg_options["options"] += " -ss " + start
+      duration -= float(start)
     length = get_data(member.guild, member, "length")
     length = min(float(length), PLAYTIME + FALLOFF) if length else PLAYTIME + FALLOFF
     if length < duration:
@@ -86,11 +109,13 @@ async def play_audio(member: discord.User, channel: discord.VoiceChannel):
     # Create discord audio player
     audio = discord.FFmpegPCMAudio(filename, **ffmpeg_options)
     audio_player = discord.PCMVolumeTransformer(audio)
-  except:
+  except Exception as e:
     print(f"{play_id}: Failed to load audio")
+    await member.send(embed = discord.Embed(title = "Failed to load your fanfare audio", description = f"Check if there is something wrong with your youtube link: {url}. Otherwise try again later.", color = 0xff0000))
     if play_ids[member.guild.id] == play_id:
       await vc.disconnect()
       print(f"{play_id}: Successfully disconnected")
+    raise e
     return
 
   print(f"{play_id}: Successfully finished loading audio")
@@ -102,11 +127,12 @@ async def play_audio(member: discord.User, channel: discord.VoiceChannel):
     print(f"{play_id}: Playing audio")
     try:
       vc.play(audio_player)
-    except:
+    except Exception as e:
       print(f"{play_id}: Failed to play audio")
       if play_ids[member.guild.id] == play_id:
         await vc.disconnect(force = True)
         print(f"{play_id}: Successfully disconnected")
+      raise e
       return
     
     # Update start time
@@ -180,3 +206,14 @@ async def save_data():
   # write_file = open()
     write_json_str = json.dumps(data)
     await write_file.write(write_json_str)
+
+
+async def send_embed(context: discord.ext.commands.Context, text: str, title: str = None, color: int = 0x0066cc):
+  embed = discord.Embed(description = text, color = color)
+  if title:
+    embed.title = title
+  try:
+    await context.send(embed = embed)
+  except discord.errors.Forbidden:
+    print(f"Missing embedded links permission for {context.channel} in {context.guild}")
+    await context.author.send(embed = discord.Embed(title="Missing Permissions", description = f"I do not have permissions to embed links in \"{context.channel}\"!", color = 0xff0000))
